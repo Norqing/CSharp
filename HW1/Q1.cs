826c602f567ff9297f571a553cb3bf71dc191ef6
Dongrong Su <116546724+Norqing@users.noreply.github.com>
2024-09-17 15:36:11 +0800

    Add files via upload
    Add the simple question answer

---
+++ HW1/Q1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Q1 
+{
+    internal class Q2
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
