6fba5e28cab4b6d4102c5350658f7aac01134458
Dongrong Su <116546724+Norqing@users.noreply.github.com>
2024-09-17 15:37:49 +0800

    Add files via upload
    Add simple question answer

---
+++ HW1/1.1\2/Q2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Q2
+{
+    internal class Q2
+    {
+        static bool IsPrime(int num)
+        {
+            if (num == 0 || num == 1) return false;
+
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+
+        static void Calculate(int num)
+        {
+            if (num == 0 || num == 1) return;
+
+            for (int i = 2; i <= num; i++)
+            {
+                if (num % i == 0 && IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入你想查询的数:");
+
+            string input = Console.ReadLine();
+
+
+            if (int.TryParse(input, out int a) && a > 0)
+            {
+                Calculate(a);
+            }
+            else
+            {
+                Console.WriteLine("请输入一个有效的正整数。");
+            }
+        }
+    }
+}
